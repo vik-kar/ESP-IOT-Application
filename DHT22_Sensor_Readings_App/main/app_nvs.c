8d1237fd0cb44372d32af50d216659fbae8cd370
Vikram Karmarkar <vikrampk2003@gmail.com>
2025-04-04 12:11:34 -0700

    NVS debugging and CSS styling for webpage

--- DHT22_Sensor_Readings_App/main/app_nvs.c
+++ DHT22_Sensor_Readings_App/main/app_nvs.c
@@ -1,14 +1,10 @@
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
-#include "esp_err.h"
-#include "esp_interface.h"
 #include "esp_log.h"
-#include "esp_wifi_types_generic.h"
-#include "nvs.h"
 #include "nvs_flash.h"
+
 #include "app_nvs.h"
 #include "wifi_app.h"
 
@@ -18,9 +14,6 @@ static const char TAG[] = "nvs";
 /* NVS namespace used for station mode credentials */
 const char app_nvs_sta_creds_namespace[] = "stacreds";
 
-/* Saves station mode WiFi Credentials to NVS 
-   @return ESP_OK if successful
-*/
 esp_err_t app_nvs_save_sta_creds(void){
 	/* create a handle to access namespace */
 	nvs_handle handle;
@@ -30,136 +23,122 @@ esp_err_t app_nvs_save_sta_creds(void){
 	*/
 	esp_err_t esp_err;
 	
-	/* Quick log */
 	ESP_LOGI(TAG, "app_nvs_save_sta_creds: Saving station mode credentials to flash");
-	
+
 	/* get the current wifi configuration */
 	wifi_config_t *wifi_sta_config = wifi_app_get_wifi_config();
-	
+
 	/* Confirm the returned pointer is not null */
-	if(wifi_sta_config){
+	if (wifi_sta_config){
 		/* Pointer is valid, begin NVS operations */
 		
 		/* open the NVS and specifically, open the namespace that we are writing to
 	       @param namespace name, mode (we did read and write), handle for the overall NVS partition	
 		*/
+		
 		esp_err = nvs_open(app_nvs_sta_creds_namespace, NVS_READWRITE, &handle);
 		
 		/* Check if there was an error opening the NVS */
-		if(esp_err != ESP_OK){
-			printf("app_nvs_save_sta_creds: Error (%s) opening NVS handle", esp_err_to_name(esp_err));
+		if (esp_err != ESP_OK){
+			printf("app_nvs_save_sta_creds: Error (%s) opening NVS handle!\n", esp_err_to_name(esp_err));
 			return esp_err;
 		}
-		
+
 		/* NVS opened successfully, so set SSID 
 		   @param handle for NVS, key in k-v pair ("ssid"), value in k-v pair (actual ssid of connection), length (MAX_SSID_LENGTH from wifi_app.h)
 		*/
 		esp_err = nvs_set_blob(handle, "ssid", wifi_sta_config->sta.ssid, MAX_SSID_LENGTH);
-		if(esp_err != ESP_OK){
-			printf("app_nvs_save_sta_creds: Error (%s) setting SSID to NVS", esp_err_to_name(esp_err));
+		if (esp_err != ESP_OK){
+			printf("app_nvs_save_sta_creds: Error (%s) setting SSID to NVS!\n", esp_err_to_name(esp_err));
+			return esp_err;
 		}
-		
-		/* Set Password - similar to what we did for setting ssid*/
+
+		/* Set Password - similar to what we did for setting ssid */
 		esp_err = nvs_set_blob(handle, "password", wifi_sta_config->sta.password, MAX_PASSWORD_LENGTH);
-		if(esp_err != ESP_OK){
-			printf("app_nvs_save_sta_creds: Error (%s) setting password to NVS", esp_err_to_name(esp_err));
+		if (esp_err != ESP_OK){
+			printf("app_nvs_save_sta_creds: Error (%s) setting Password to NVS!\n", esp_err_to_name(esp_err));
+			return esp_err;
 		}
-		
+
 		/* Commit credentials to NVS */
 		esp_err = nvs_commit(handle);
-		
-		/* Error checking from nvs_commit operation */
-		if(esp_err != ESP_OK){
-			printf("app_nvs_save_sta_creds: Error (%s) committing credentials to NVS", esp_err_to_name(esp_err));
+		if (esp_err != ESP_OK){
+			printf("app_nvs_save_sta_creds: Error (%s) comitting credentials to NVS!\n", esp_err_to_name(esp_err));
+			return esp_err;
 		}
 		
-		/* Close NVS */
 		nvs_close(handle);
-		
 		ESP_LOGI(TAG, "app_nvs_save_sta_creds: wrote wifi_sta_config: Station SSID: %s Password: %s", wifi_sta_config->sta.ssid, wifi_sta_config->sta.password);
 	}
-	ESP_LOGI(TAG, "app_nvs_save_sta_creds: returned ESP_OK\n");
+
+	printf("app_nvs_save_sta_creds: returned ESP_OK\n");
 	return ESP_OK;
 }
 
 /* Loads prev saved credentials from NVS
    @return TRUE if prev saved credentials were found
 */
-bool app_nvs_load_sta_creds(void){
+bool app_nvs_load_sta_creds(void)
+{
 	/* Create an NVS handle */
 	nvs_handle handle;
 	
 	/* create an error type to check successful function returns */
 	esp_err_t esp_err;
-	
+
 	ESP_LOGI(TAG, "app_nvs_load_sta_creds: loading WiFi credentials from flash (from NVS)");
-	
+
 	/* Check if we can successfully open the NVS and our specific namespace in there */
-	if(nvs_open(app_nvs_sta_creds_namespace, NVS_READONLY, &handle) == ESP_OK){
+	if (nvs_open(app_nvs_sta_creds_namespace, NVS_READONLY, &handle) == ESP_OK){
 		wifi_config_t *wifi_sta_config = wifi_app_get_wifi_config();
-		
-		/* check if we even have a current configuration */
-		if(wifi_sta_config == NULL){
-			/* allocate memory for wifi_sta_config, to fill it with the data we get from NVS later */
-			wifi_sta_config = (wifi_config_t*) malloc(sizeof(wifi_config_t));
-		}
 		
 		/* clear existing configurations since our SSID and pwd will come from NVS */
 		memset(wifi_sta_config, 0x00, sizeof(wifi_config_t));
-		
+
 		/* Allocate a buffer that we'll write the NVS data to */
 		size_t wifi_config_size = sizeof(wifi_config_t);
 		
 		/* uint8_t specifies that each byte of the allocated memory is to be treated as an unsigned 8-bit integer - meaning we can access the allocated memory byte by byte */
-		uint8_t *wifi_config_buffer = (uint8_t *) malloc(wifi_config_size);
+		uint8_t *wifi_config_buff = (uint8_t*)malloc(sizeof(uint8_t) * wifi_config_size);
 		
 		/* Zero-out the buffer */
-		memset(wifi_config_buffer, 0x00, sizeof(wifi_config_size));
-		
+		memset(wifi_config_buff, 0x00, sizeof(wifi_config_size));
+
 		/* Load the details we need */
 		
 		/* get the size of the ssid field in our zero-d out wifi config struct */
-		size_t wifi_sta_ssid_size = sizeof(wifi_sta_config->sta.ssid);
+		wifi_config_size = sizeof(wifi_sta_config->sta.ssid);
 		
 		/* copy the desired info into wifi_config_buffer */
-		esp_err = nvs_get_blob(handle, "ssid", wifi_config_buffer, &wifi_sta_ssid_size);
+		esp_err = nvs_get_blob(handle, "ssid", wifi_config_buff, &wifi_config_size);
 		
-		if(esp_err != ESP_OK){
+		if (esp_err != ESP_OK){
 			/* Operation failed, do memory cleanup */
-			free(wifi_config_buffer);
-			free(wifi_sta_config);
-			printf("app_nvs_load_sta_creds: (%s) no station SSID found in NVS", esp_err_to_name(esp_err));
+			free(wifi_config_buff);
+			printf("app_nvs_load_sta_creds: (%s) no station SSID found in NVS\n", esp_err_to_name(esp_err));
 			return false;
 		}
 		
 		/* copy from buffer and into wifi_config_struct (specifically into the sta.ssid area) */
-		memcpy(wifi_sta_config->sta.ssid, wifi_config_buffer, wifi_sta_ssid_size);
-		
+		memcpy(wifi_sta_config->sta.ssid, wifi_config_buff, wifi_config_size);
+
 		/* Similar routine for loading password */
-		size_t wifi_sta_pwd_size = sizeof(wifi_sta_config->sta.password);
-		
-		/* Get the desired info */
-		esp_err = nvs_get_blob(handle, "password", wifi_config_buffer, &wifi_sta_pwd_size);
-		if(esp_err != ESP_OK){
-			/* Operation failed, do memory cleanup */
-			free(wifi_config_buffer);
-			free(wifi_sta_config);
-			printf("app_nvs_load_sta_creds: (%s) no station password found in NVS", esp_err_to_name(esp_err));
+		wifi_config_size = sizeof(wifi_sta_config->sta.password);
+		esp_err = nvs_get_blob(handle, "password", wifi_config_buff, &wifi_config_size);
+		if (esp_err != ESP_OK){
+			free(wifi_config_buff);
+			printf("app_nvs_load_sta_creds: (%s) retrieving password!\n", esp_err_to_name(esp_err));
 			return false;
 		}
-		
-		/* copy from buffer into wifi config struct */
-		memcpy(wifi_sta_config->sta.password, wifi_config_buffer, wifi_sta_pwd_size);
-		
-		/* After successful copying, we can free allocated memory */
-		free(wifi_config_buffer);
+		memcpy(wifi_sta_config->sta.password, wifi_config_buff, wifi_config_size);
+
+		free(wifi_config_buff);
 		nvs_close(handle);
-		
+
 		printf("app_nvs_load_sta_creds: SSID: %s Password: %s\n", wifi_sta_config->sta.ssid, wifi_sta_config->sta.password);
 		
 		/* return the first character of SSID. If != NULL, then we know it returned true (ie the SSID is loaded) */
 		return wifi_sta_config->sta.ssid[0] != '\0';
-		
 	}
 	else{
 		return false;
@@ -169,37 +148,75 @@ bool app_nvs_load_sta_creds(void){
 /* Clears station mode credentials from NVS
    @return ESP_OK if successful
 */
-esp_err_t app_nvs_clear_sta_creds(void){
+esp_err_t app_nvs_clear_sta_creds(void) {
 	nvs_handle handle;
 	esp_err_t esp_err;
-	
-	ESP_LOGI(TAG, "app_nvs_clear_sta_creds: Clearing WiFi station mode credentials from flash");
-	
+	ESP_LOGI(TAG, "app_nvs_clear_sta_creds: Clearing Wifi station mode credentials from flash");
+
 	esp_err = nvs_open(app_nvs_sta_creds_namespace, NVS_READWRITE, &handle);
-	if(esp_err != ESP_OK){
-		printf("app_nvs_clear_sta_creds: Error (%s) opening NVS handle", esp_err_to_name(esp_err));
+	if (esp_err != ESP_OK){
+		printf("app_nvs_clear_sta_creds: Error (%s) opening NVS handle!\n", esp_err_to_name(esp_err));
 		return esp_err;
 	}
-	
-	/* Proceed to erase credentials */
+
+	// Erase credentials
 	esp_err = nvs_erase_all(handle);
-	
-	if(esp_err != ESP_OK){
-		printf("app_nvs_clear_sta_creds: Error (%s) while erasing station mode credentials", esp_err_to_name(esp_err));
-		return esp_err;
-	}	
-	
-	/* Commit the clearing of credentials from NVS */
-	esp_err = nvs_commit(handle);
-	if(esp_err != ESP_OK){
-		printf("app_nvs_clear_sta_creds: Error (%s) in nvs_commit", esp_err_to_name(esp_err));
+	if (esp_err != ESP_OK){
+		printf("app_nvs_clear_sta_creds: Error (%s) erasing station mode credentials!\n", esp_err_to_name(esp_err));
 		return esp_err;
 	}
-	
+
+	// Commit clearing credentials from NVS
+	esp_err = nvs_commit(handle);
+	if (esp_err != ESP_OK){
+		printf("app_nvs_clear_sta_creds: Error (%s) NVS commit!\n", esp_err_to_name(esp_err));
+		return esp_err;
+	}
 	nvs_close(handle);
-	printf("app_nvs_clear_sta_creds: Returned ESP_OK");
+
+	printf("app_nvs_clear_sta_creds: returned ESP_OK\n");
 	return ESP_OK;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-Source code for an IoT Project using the ESP32 microcontroller and ESP-IDF.
+### ESP32 IoT Framework
 
-Sensor Integration: DHT22
+**Project Overview:**
+This project presents a robust Internet of Things (IoT) framework developed on the ESP32 platform, tailored to demonstrate advanced capabilities in network and device management, data collection, and integration with cloud services. It leverages the powerful features of the ESP32 microcontroller to provide a comprehensive suite of functionalities that cater to modern IoT applications.
 
-WiFi Connectivity
+**Key Features:**
 
-Cloud Integration
+- **WiFi Connectivity Management**: Implements robust mechanisms for managing WiFi operations, including capabilities for the ESP32 to act as both a station and an access point. The system handles automatic reconnections, credential management, and user-initiated disconnects and resets through physical interactions.
 
-Real-Time Data Processing
+- **HTTP Server and Web Interface**: Features a built-in HTTP server that hosts a web interface for device interaction. This interface allows users to configure device settings, monitor real-time data, and manage device functionalities remotely. It supports OTA firmware updates, providing a mechanism for updating the ESP32's firmware directly through the web interface.
 
-Utilization of FreeRTOS
+- **Non-Volatile Storage (NVS) Handling**: Utilizes the ESP32’s NVS to store and retrieve configurations across reboots, ensuring device settings like WiFi credentials are preserved. This feature enhances the device's usability in real-world applications where power cycles are common.
+
+- **Time Synchronization**: Integrates SNTP (Simple Network Time Protocol) to ensure the device maintains accurate time, which is critical for logging and scheduling tasks accurately.
+
+- **AWS IoT Integration**: Connects seamlessly with AWS IoT Core to publish and subscribe to MQTT topics, enabling the device to interact with cloud-based services and applications. This integration allows for the remote monitoring and management of device operations and sensor data.
+
+- **Sensor Data Management**: While specific sensors like DHT22 are implied, the framework is designed to be adaptable to various sensors for environmental monitoring or other IoT applications, demonstrating the collection, processing, and dissemination of sensor data.
+
+**Technologies Used:**
+- ESP-IDF (Espresso IoT Development Framework)
+- MQTT for messaging
+- AWS IoT for cloud integration
+- HTML/CSS/JavaScript for the web interface
+
+**Project Significance:**
+This project exemplifies the use of embedded systems to create interconnected, easily manageable IoT devices that can be monitored and controlled remotely. It showcases the ESP32’s capabilities in handling multiple tasks such as network management, user interaction through a web server, and real-time data handling, making it an excellent showcase of skill in both software and hardware aspects of IoT development.
